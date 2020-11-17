c84d1494d10672ff2a3fdd891a6b3204fa2e2a3e
bensmithkid28 <60799599+bensmithkid28@users.noreply.github.com>
2020-11-16 20:46:57 -0600

    complete activity 9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,112 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Register from './components/Register/Register';
+import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
-    data: null
+    data: null,
+    token: null,
+    user: null
+  }
+
+  authenticateUser = () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      localStorage.removeItem('user')
+      this.setState({ user: null });
+    }
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      }
+      axios.get('http://localhost:5000/api/auth', config)
+        .then((response) => {
+          localStorage.setItem('user', response.data.name);
+          this.setState({ user: response.data.name });
+        })
+        .catch((error) => {
+          localStorage.removeItem('user');
+          this.setState({ user: null });
+          console.log(`Error logging in: ${error}`);
+        })
+    }
+  }
+
+  logOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({ user: null, token: null });
   }
 
   componentDidMount() {
     axios.get('http://localhost:5000')
-    .then((response) => {
-      this.setState({
-        data: response.data
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
       })
-    })
-    .catch((error) => {
-      console.error(`Error fetching data: ${error}`);
-    })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      })
+
+      this.authenticateUser();
   }
-  
   render() {
+    let { user, data } = this.state;
+    const authProps = {
+      authenticateUser: this.authenticateUser,
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          GoodThings
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>GoodThings</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+              <li>
+                {user ?
+                  <Link to="" onClick={this.logOut}>Log out</Link> :
+                  <Link to="/login">Log in</Link>
+                }
+              </li>
+            </ul>
+          </header>
+          <main>
+            <Route exact path="/">
+              {user ?
+                <React.Fragment>
+                  <div>Hello {user}!</div>
+                  <div>{data}</div>
+                </React.Fragment> :
+                <React.Fragment>
+                  Please Register or Login
+                </React.Fragment>
+              }
+
+            </Route>
+            <Switch>
+              <Route 
+                exact path="/register" 
+                render={() => <Register {...authProps} />} />
+              <Route 
+                exact path="/login" 
+                render={() => <Login {...authProps} />} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
--- server.js
+++ server.js
@@ -3,10 +3,13 @@ import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
 import bcrypt from 'bcryptjs';
+import User from './models/User';
 import jwt from 'jsonwebtoken';
 import config from 'config';
-import User from '.models/User';
+import auth from './middleware/auth';
+import Post from './models/Post';
 
+// Initialize express application
 const app = express();
 
 
@@ -19,80 +22,260 @@ app.use(
     })
 );
 
+/**
+ * @route GET api/auth
+ * @desc Authenticate user
+ */
+
+app.get('/api/auth', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send('Unknown server error');
+    }
+});
 
 /**
- * @route GET/
- * @desc Test endpoint
+ * @route GET api/posts
+ * @desc Get posts
  */
-app.get('/', (req,res) =>
-    res.send('http get request sent to root api endpoint')
-);
+app.get('/api/posts', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+
+        res.json(posts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
 /**
- * @route POST api/users
- * @desc Register user
+ * @route GET api/posts/:id
+ * @desc Get post
+ */
+app.get('/api/posts/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found'});
+        }
+
+        res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route POST api/login
+ * @desc Login user
  */
 app.post(
-    '/api/users',
+    '/api/login',
     [
-      
-        check('name', 'Please enter your name').not().isEmpty(),
-       
         check('email', 'Please enter a valid email').isEmail(),
-        
-        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+        check('password', 'A password is required').exists()
     ],
-    async(req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(422).json({ errors: errors.array() });
         } else {
-            const { name, email, password} = req.body;
+            const { email, password } = req.body;
             try {
-                
-                let user = await User.findOne({email: email});
-                if (user){
+                let user = await User.findOne({ email: email });
+                if (!user) {
                     return res
-                    .status(400)
-                    .json({ errors: [{msg: 'That user already exists' }] });
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
                 }
 
-              
-                user = new User({
-                    name: name,
-                    email: email,
-                    password: password
-                
-                });
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+                }
 
-        
-                const salt = await bcrypt.genSalt(10);
-                user.password = await bcrypt.hash(password, salt);
-
-                
-                await user.save();
-                res.send('User successfully registered');
-                const payload = {
-                    user: {
-                        id: user.id
-                    }
-                };
-
-                jwt.sign(
-                    payload,
-                    config.get('jwtSecret'),
-                    { expiresIn: '10hr'},
-                    (err, token) => {
-                        if (err) throw err;
-                        res.json({ token: token });
-                    }
-                );  
+                returnToken(user, res);
             } catch (error) {
-                res.status(500).send('Server Error')
+                res.status(500).send('Server error');
             }
         }
     }
 );
 
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 
+app.get('/', (req, res) =>
+    res.send('http get request sent to root api endpoint')
+);
+
+/**
+ * @route POST api/users
+ * @desc Register user
+
+ */
+
+ 
+app.post('/api/users', 
+ 
+[  check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { name, email, password } = req.body;
+            try {
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'User already exists'}] });
+                }
+        
+                
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+                
+                await user.save();
+
+                returnToken(user, res);
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
+/**
+ * @route POST api/posts
+ * @desc Create post
+ */
+app.post(
+    '/api/posts',
+    [
+        auth,
+        [
+            check('title', 'Title text is required')
+                .not()
+                .isEmpty(),
+            check('body', 'Body text is required')
+                .not()
+                .isEmpty(),
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({errors: errors.array() });
+        } else {
+            const { title, body } = req.body;
+            try {
+                const user = await User.findById(req.user.id);
+
+                const post = new Post({
+                    user: user.id,
+                    title: title,
+                    body: body
+                });
+
+                await post.save();
+
+                res.json(post);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+app.delete('/api/posts/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found'});
+        }
+
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized'});
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Post removed' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route PUT api/posts/:id
+ * @desc Update a post
+ */
+app.put('/api/posts/:id', auth, async (req, res) => {
+    try {
+        const { title, body } = req.body;
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found'});
+        }
+
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized'});
+        }
+
+        post.title = title || post.title;
+        post.body = body || post.body;
+
+        await post.save();
+
+        res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+const returnToken = (user, res) => {
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+
+    jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '10hr' },
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token: token });
+        }
+    );
+};
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
